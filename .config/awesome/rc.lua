4fb79ae4f62a9c44558afde4f9f15ef7214f765d
Clément Delafargue <divarvel@gmail.com>
2011-08-23 23:47:10 +0200

    Add: suit.fair > suit.tile

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -26,17 +26,16 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.floating,
+   -- awful.layout.suit.tile,
+   awful.layout.suit.tile.left,
+   -- awful.layout.suit.tile.bottom,
+   -- awful.layout.suit.tile.top,
+   -- awful.layout.suit.spiral,
+   -- awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max, awful.layout.suit.max.fullscreen,
+   -- awful.layout.suit.floating,
     awful.layout.suit.magnifier
 }
 -- }}}
