ab8d00e86729f29c354b68d2e87240401c8768ed
Clément Delafargue <clement@Delafargue.name>
2011-12-14 07:13:51 +0100

    Add: Updated layouts and tags in awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -26,17 +26,16 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.floating,
+   -- awful.layout.suit.tile,
+   awful.layout.suit.tile.left,
+   -- awful.layout.suit.tile.bottom,
+   -- awful.layout.suit.tile.top,
+   -- awful.layout.suit.spiral,
+   -- awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max, awful.layout.suit.max.fullscreen,
+   -- awful.layout.suit.floating,
     awful.layout.suit.magnifier
 }
 -- }}}
@@ -44,8 +43,8 @@ layouts =
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {
-  names = { "www", "code", "ssh", "social", "email","music", "movies", "gimp" },
-  layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1] }
+  names = { "www", "code", "ssh", "social", "email", "music", "movies", "documents", "paludis" },
+  layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1] }
 }
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
