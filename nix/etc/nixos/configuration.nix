d8aa97a3695a4d7476323b71db28e938e43679ba
Clement Delafargue <clement.delafargue@fretlink.com>
2018-07-20 16:52:19 +0200

    Add nix configuration

---
+++ nix/etc/nixos/configuration.nix
@@ -0,0 +1,103 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+
+  boot.initrd.luks.devices = [
+    {
+       name = "root";
+       device = "/dev/disk/by-uuid/8550b8b0-692f-47cf-9ccf-fac206bcf328";
+       preLVM = true;
+       allowDiscards = true;
+    }
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "fr-bepo";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.variables.EDITOR = pkgs.lib.mkOverride 10 "vim";
+
+  environment.systemPackages = with pkgs; [
+    wget vim i3 stack git firefox i3lock rofi kakoune vscode slack networkmanager gnupg pass gnumake stow
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "fr";
+  services.xserver.xkbVariant = "bepo";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.naturalScrolling = true;
+
+  services.xserver.windowManager.i3.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.clementd = {
+    isNormalUser = true;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
+
+}
